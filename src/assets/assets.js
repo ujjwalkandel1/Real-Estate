45a6c5d1dbc37b1687e98a9d7fbc2206d750e0ae
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2024-12-30 23:34:56 +0545

    first phase done

---
+++ src/assets/assets.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+  {
+      title: "Himalayan Haven",
+      price: "रु 25,00,000",
+      location: "Kathmandu",
+      image: project_img_1
+  },
+  {
+      title: "Everest Estate",
+      price: "रु 30,00,000",
+      location: "Solukhumbu",
+      image: project_img_2
+  },
+  {
+      title: "Pokhara Paradise",
+      price: "रु 20,00,000",
+      location: "Pokhara",
+      image: project_img_3
+  },
+  {
+      title: "Chitwan Charm",
+      price: "रु 18,00,000",
+      location: "Chitwan",
+      image: project_img_4
+  },
+  {
+      title: "Lumbini Lux",
+      price: "रु 22,00,000",
+      location: "Lumbini",
+      image: project_img_5
+  },
+  {
+      title: "Janakpur Jewel",
+      price: "रु 15,00,000",
+      location: "Janakpur",
+      image: project_img_6
+  }
+];
+
+
+export const testimonialsData = [
+  {
+      name: "Santosh Sharma",
+      title: "Business Owner",
+      image: profile_img_1,
+      alt: "Portrait of Santosh Sharma",
+      rating: 5,
+      text: "From the very first meeting, they understood my vision and helped me find the perfect property in Kathmandu. Their attention to detail and commitment to client satisfaction is unmatched."
+  },
+  {
+      name: "Priyanka Koirala",
+      title: "Freelance Designer",
+      image: profile_img_2,
+      alt: "Portrait of Priyanka Koirala",
+      rating: 4,
+      text: "I was looking for a serene property in Pokhara, and they delivered beyond my expectations. Their dedication and professionalism are truly commendable."
+  },
+  {
+      name: "Rajan Gurung",
+      title: "Entrepreneur",
+      image: profile_img_3,
+      alt: "Portrait of Rajan Gurung",
+      rating: 5,
+      text: "Their expertise helped me secure an amazing property in Chitwan. Their team is knowledgeable, friendly, and very helpful throughout the process."
+  }
+];
---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+        <h1>Project <span>Completed</span></h1>
+        <p>Crafting Spaces, Building Legacies-Explore Our Porfolio</p>
+    </div>
+  )
+}
+
+export default Projects
